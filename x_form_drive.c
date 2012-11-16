4b09efcb985c3235ddc86ffc578628b1f9a27baf
Luke Liu <lllluuukke@gmail.com>
2012-11-16 00:21:01 -0600

    Experiment 3 failed. Added experiment 4, advanced algorithm.

--- x_form_drive.c
+++ x_form_drive.c
@@ -12,6 +12,7 @@
 #define SIN45 .8509
 #define MIN(X,Y) ((X) < (Y) ? (X) : (Y))
 #define MAX(X,Y) ((X) > (Y) ? (X) : (Y))
+#define PW2(x) ((x)*(x))
 
 int struggle(int a, int b) {
   if(abs(a*b) != a*b || a*b == 0)
@@ -25,29 +26,77 @@ int struggle(int a, int b) {
   }
 }
 
+int sign(float v) {
+  if(v > 0)
+    return 1;
+  else if(v < 0)
+    return -1;
+}
+
+// Determine the direction based on wheel orientation and control input
+int cal(int quad) {
+  float tmp = 0;
+  switch(quad) {
+	// Motor_NE, Quadrant I
+    case 1:
+	  tmp = sin(atan(vexRT[Ch3]/vexRT[Ch4])-PI/4);
+	  return sign(tmp);
+	  break;
+	// Motor_NW, Quadrant II
+    case 2:
+	  tmp = cos(atan(vexRT[Ch3]/vexRT[Ch4])+3*PI/4);
+	  return sign(tmp);
+	  break;
+	// Motor_SW, Quadrant III
+    case 3:
+	  tmp = cos(atan(vexRT[Ch3]/vexRT[Ch4])+I/4);
+	  return sign(tmp);
+	  break;
+	// Motor_SE, Quadrant IV
+    case 4:
+	  tmp = sin(atan(vexRT[Ch3]/vexRT[Ch4])+PI/4);
+	  return sign(tmp);
+	  break;
+  }
+}
+
 task main() {
   while(!SensorValue[touch]) {
-    // EXPERIMENTAL TWO!!!
-    motor[motor_ne] =
-	    struggle((int)(((float)(vexRT[ch3]-vexRT[ch4]))*SIN45), -1*vexRT[ch1]);
-    motor[motor_se] =
-	    struggle((int)(((float)(vexRT[ch4]+vexRT[ch3]))*SIN45), -1*vexRT[ch1]);
-    motor[motor_sw] =
-	    struggle((int)(((float)(vexRT[ch4]-vexRT[ch3]))*SIN45), -1*vexRT[ch1]);
-    motor[motor_nw] =
-	    struggle((int)(((float)-1*(vexRT[ch4]+vexRT[ch3]))*SIN45), -1*vexRT[ch1]);
-    // EXPERIMENTAL THREE!!!
-    motor[motor_ne] =
-struggle((int)(((float)(vexRT[ch3]-vexRT[ch4]))*sin(atan((float)vexRT[ch4]/
-(float)vexRT[ch3]))), -1*vexRT[ch1]);
-    motor[motor_se] =
-struggle((int)(((float)(vexRT[ch4]+vexRT[ch3]))*sin(atan((float)vexRT[ch4]/
-(float)vexRT[ch3]))), -1*vexRT[ch1]);
-    motor[motor_sw] =
-struggle((int)(((float)(vexRT[ch4]-vexRT[ch3]))*sin(atan((float)vexRT[ch4]/
-(float)vexRT[ch3]))), -1*vexRT[ch1]);
-    motor[motor_nw] =
-struggle((int)(((float)-1*(vexRT[ch4]+vexRT[ch3]))*sin(atan((float)vexRT[ch4]/
-(float)vexRT[ch3]))), -1*vexRT[ch1]);
+    if(vexRT[Btn5U] || vexRT[Btn5D] || vexRT[Btn6U] || vexRT[Btn6D] ||
+      vexRT[Btn7U] || vexRT[Btn7R] || vexRT[Btn7D] || vexRT[Btn7L] ||
+      vexRT[Btn8U] || vexRT[Btn8R] || vexRT[Btn8D] || vexRT[Btn8L]) {
+      motor[motor_ne] = 0;
+      motor[motor_ne] = 0;
+      motor[motor_ne] = 0;
+      motor[motor_ne] = 0;
+   }
+   else {
+     // EXPERIMENT TWO!!!
+     motor[motor_ne] =
+	   struggle((int)(((float)(vexRT[Ch3]-vexRT[Ch4]))*SIN45),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_se] =
+	   struggle((int)(((float)(vexRT[Ch4]+vexRT[Ch3]))*SIN45),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_sw] =
+	   struggle((int)(((float)(vexRT[Ch4]-vexRT[Ch3]))*SIN45),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_nw] =
+	   struggle((int)(((float)-1*(vexRT[Ch4]+vexRT[Ch3]))*SIN45),
+	   -1*vexRT[Ch1]/2);
+     // EXPERIMENT FOUR!!!
+     motor[motor_ne] =
+	   struggle(cal(1)*(int)sqrt(PW2(vexRT[Ch3])+PW2(vexRT[Ch4])),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_se] =
+	   struggle(cal(4)*(int)sqrt(PW2(vexRT[Ch3])+PW2(vexRT[Ch4])),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_sw] =
+	   struggle(cal(3)*(int)sqrt(PW2(vexRT[Ch3])+PW2(vexRT[Ch4])),
+	   -1*vexRT[Ch1]/2);
+     motor[motor_nw] =
+	   struggle(cal(2)*(int)sqrt(PW2(vexRT[Ch3])+PW2(vexRT[Ch4])),
+	   -1*vexRT[Ch1]/2);
+	 }
   }
 }
